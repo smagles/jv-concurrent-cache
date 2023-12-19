2e0c848d4dc21e5c70f933d329773f517721ac0c
boroda4436 <chupika.bohdan@ukr.net>
2023-12-19 17:37:14 +0200

    added pom.xml

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,53 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private final Map<K, V> cache = new HashMap<>();
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final java.util.concurrent.locks.Lock readLock = lock.readLock();
+    private final java.util.concurrent.locks.Lock writeLock = lock.writeLock();
+
     public V get(K key) {
-        return null;
+        readLock.lock();
+        try {
+            return cache.get(key);
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        writeLock.lock();
+        try {
+            cache.put(key, value);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void remove(K key) {
-
+        writeLock.lock();
+        try {
+            cache.remove(key);
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public void clear() {
-
+        writeLock.lock();
+        try {
+            cache.clear();
+        } finally {
+            writeLock.unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        return cache.size();
     }
 }
