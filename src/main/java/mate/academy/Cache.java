588b6890eb6453d605dde3b2f90c76524b1e7c21
smagles <lesialist@icloud.com>
2025-08-01 14:12:54 +0200

    Implement the concurrent cache

--- src/main/java/mate/academy/Cache.java
+++ src/main/java/mate/academy/Cache.java
@@ -1,23 +1,56 @@
 package mate.academy;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Cache<K, V> {
+    private Map<K, V> cache = new HashMap<>();
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+
     public V get(K key) {
-        return null;
+        lock.readLock().lock();
+        try {
+            return cache.get(key);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public void put(K key, V value) {
-
+        lock.writeLock().lock();
+        try {
+            cache.put(key, value);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void remove(K key) {
-
+        lock.writeLock().lock();
+        try {
+            cache.remove(key);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public void clear() {
-
+        lock.writeLock().lock();
+        try {
+            cache.clear();
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public int size() {
-        return -1;
+        lock.readLock().lock();
+        try {
+            return cache.size();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
